8eb6c5b72e471b4099949c24bd12faa95ca1a9c1
Saad Imran <233966528+saad-imran2891@users.noreply.github.com>
2026-09-14 22:48:47 +0500

    Clean repository files

--- day3.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-img = cv2.imread("test.jpg")
-
-print("Shape :", img.shape)
-print("Height :", img.shape[0])
-print("Width :", img.shape[1])
-print("Channels :", img.shape[2])
-print("Data Type :", img.dtype)
